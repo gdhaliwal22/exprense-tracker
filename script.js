cdc7f8da7b7dc65fa49071b016964a6e25fbc4f2
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-17 13:10:22 -0700

    add updateLocalStorage function to the addTransaction function

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-];
+// const dummyTransactions = [
+//   { id: 1, text: "Flower", amount: -20 },
+//   { id: 2, text: "Salary", amount: 300 },
+//   { id: 3, text: "Book", amount: -10 },
+//   { id: 4, text: "Camera", amount: 150 },
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
 // Add transaction
 function addTransaction(e) {
@@ -30,6 +35,7 @@ function addTransaction(e) {
     transactions.push(transaction);
     addTransactionDOM(transaction);
     updateValues();
+    updateLocalStorage();
     text.value = "";
     amount.value = "";
   }
@@ -85,6 +91,11 @@ function updateValues() {
   money_minus.innerText = `$${expense}`;
 
   console.log(expense);
+}
+
+//Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 // Init app
 function init() {
